3000617867a555c209d9ac088e8a01935dc995de
lampo1024 <lampo1024@gmail.com>
2019-03-26 23:00:45 +0800

    Init project

---
+++ VisitorCounterDemo/Middleware/VisitorCounterMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using VisitorCounter.Util;
+
+namespace VisitorCounter.Middleware
+{
+    public class VisitorCounterMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        public VisitorCounterMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var visitorId = context.Request.Cookies["VisitorId"];
+            
+            if (visitorId == null)
+            {
+                visitorId = Guid.NewGuid().ToString();
+                //don the necessary staffs here to save the count by one
+                context.Response.Cookies.Append("VisitorId", visitorId, new CookieOptions()
+                {
+                    Path = "/",
+                    HttpOnly = true,
+                    Secure = false,
+                    Expires = DateTime.Now.AddMonths(1)
+                });
+            }
+            VisitCounter.Visit(visitorId);
+
+            await _requestDelegate(context);
+        }
+    }
+
+    public static class UseVisitorCounterExtension
+    {
+        public static IApplicationBuilder UseVisitorCounter(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<VisitorCounterMiddleware>();
+        }
+    }
+}
---
+++ VisitorCounterDemo/Util/VisitorCounter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace VisitorCounter.Util
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class VisitCounter
+    {
+        static VisitCounter()
+        {
+            var timer = new Timer(10000) { Enabled = true };
+            timer.Start();
+            timer.Elapsed += (sender, e) => VisitorList.RemoveWhere(x => x.LatestVisitAt < DateTime.Now.AddMinutes(-5));
+        }
+
+        public static void Visit(string visitorId)
+        {
+            var visitor = VisitorList.FirstOrDefault(x => x.Id == visitorId);
+            if (visitor == null)
+            {
+                VisitorList.Add(new Visitor
+                {
+                    Id = visitorId,
+                    FirstVisitAt = DateTime.Now,
+                    LatestVisitAt = DateTime.Now
+                });
+            }
+            else
+            {
+                visitor.LatestVisitAt = DateTime.Now;
+            }
+        }
+
+        public static int VisitorNumber => VisitorList.Count;
+
+        public static HashSet<Visitor> VisitorList { get; } = new HashSet<Visitor>();
+    }
+
+    /// <summary>
+    /// Visitor
+    /// </summary>
+    public class Visitor
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// first visit time
+        /// </summary>
+        public DateTime FirstVisitAt { get; set; }
+        /// <summary>
+        /// latest visit time
+        /// </summary>
+        public DateTime LatestVisitAt { get; set; }
+    }
+}
